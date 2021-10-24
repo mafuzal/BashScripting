feb898cd22db6409214cbc99918482569923b069
mafuzalhoque <mafuzalhoque@gmail.com>
2021-10-24 08:51:03 -0400

    BashScripting: Variable Inputs PrintOut

---
+++ 1_ShellScripting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+MY_SHELL="BASH"
+echo "I like the $MY_SHELL shell."
+echo "I like the ${MY_SHELL} shell."
+echo "I like ${MY_SHELL}ing."
+echo "I like $MY_SHELLing."
+SERVER_NAME=$(hostname)
+echo "You are running this script on ${SERVER_NAME}."
+echo "An older way of enclosing the commands in back ticks which is replaced by Dollar-Sign parentheses"
+SERVER_NAME=`hostname`
+echo "You are running this script on ${SERVER_NAME}."
